0521b6d2fed782a943346720c8cb618df15cd3be
Pavlo Malyshkin <pmalyshkin@gmail.com>
2021-08-01 00:14:45 +0300

    adds test for private method

--- tests/Life.test.ts
+++ tests/Life.test.ts
@@ -9,6 +9,9 @@ describe('Life', () => {
   // generations presets
   const gens = require('./gens.json')
 
+  /**
+   * Public methods
+   */
   describe('nextGen', () => {
     it('should return expected generation and hash for cache access', () => {
       for (const state of gens) {
@@ -38,6 +41,20 @@ describe('Life', () => {
       const gen = life.getGen('abc')
 
       expect(gen).toMatchObject([[]])
+    })
+  })
+
+  /**
+   * Private methods
+   * 
+   * We cann't access to private methods directly, like `life.priv()`
+   * but we can use array access ([]) to get at the private members: `life['priv']()
+   */
+  describe('getGenHash', () => {
+    it("should return md5 hash 'e0412ae31ac56c590b686575eb552a20'", () => {
+      const md5 = life['getGenHash']( [[0,0],[1,1]] )
+
+      expect(md5).toEqual('e0412ae31ac56c590b686575eb552a20')
     })
   })
 })
